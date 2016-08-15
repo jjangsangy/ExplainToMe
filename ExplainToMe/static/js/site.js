3974753bb8e5f86aeea194208a63ce738669ebf5
Sang Han <jjangsangy@gmail.com>
2016-08-15 10:41:12 -0700

    added opengraph links

--- ExplainToMe/static/js/site.js
+++ ExplainToMe/static/js/site.js
@@ -2,10 +2,9 @@
     'use strict';
     var label = Array.from(document.body.querySelectorAll("label"));
     label.forEach(function(item) {
-            item.className = 'sr-only';
-        }
-    )
-    document.getElementById("input-form").addEventListener("submit", function (e) {
+        item.className = 'sr-only';
+    })
+    document.getElementById("input-form").addEventListener("submit", function(e) {
         $("#input-form").fadeOut("slow");
         $("#loader").fadeIn("slow");
     });

f26e6ec0ca3d7e5fa26046982257e6e48fc973e6
Sang Han <jjangsangy@gmail.com>
2016-08-15 08:47:14 -0700

    javascript module

--- ExplainToMe/static/js/site.js
+++ ExplainToMe/static/js/site.js
@@ -1,12 +1,12 @@
-'use strict';
-
-document.querySelectorAll("label").forEach(
-    function(item) {
-        item.className = 'sr-only';
-    }
-);
-
-document.getElementById("input-form").addEventListener("submit", function (e) {
-    $("#input-form").fadeOut("slow");
-    $("#loader").fadeIn("slow");
-});
+var ExplainToMe = (function(document) {
+    'use strict';
+    document.querySelectorAll("label").forEach(
+        function(item) {
+            item.className = 'sr-only';
+        }
+    );
+    document.getElementById("input-form").addEventListener("submit", function (e) {
+        $("#input-form").fadeOut("slow");
+        $("#loader").fadeIn("slow");
+    });
+})(document);

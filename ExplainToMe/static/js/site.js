128486c43a6cfef367a15a6ca1277d09aafc27e8
Sang Han <jjangsangy@gmail.com>
2016-08-16 05:43:17 -0700

    es

--- ExplainToMe/static/js/site.js
+++ ExplainToMe/static/js/site.js
@@ -1,9 +1,10 @@
-let label = Array.from(document.body.querySelectorAll('label'));
-label.forEach(function (items) {
-    items.className = 'sr-only';
-});
-
-document.getElementById('input-form').addEventListener('submit', function (e) {
-    $('#input-form').fadeOut('slow');
-    $('#loader').fadeIn('slow');
-});
+(function () {
+    let label = Array.from(document.body.querySelectorAll('label'));
+    label.forEach(function (items) {
+        items.className = 'sr-only';
+    });
+    document.getElementById('input-form').addEventListener('submit', function (e) {
+        $('#input-form').fadeOut('slow');
+        $('#loader').fadeIn('slow');
+    })
+})();

339f4ce5dc3fea9ad34e0d93a2eb2c144b22230e
Sang Han <jjangsangy@gmail.com>
2018-07-25 16:10:22 -0700

    fixed posted code json

--- ExplainToMe/views/api.py
+++ ExplainToMe/views/api.py
@@ -7,9 +7,13 @@ api = Blueprint('api', __name__)
 
 @api.route('/summary', methods=['POST'])
 def summary():
-    url= request.values.get('url',
-        default='https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
-    max_sent = request.values.get('max_sent', 10)
+    posted = request.get_json()
+    if posted:
+        url = posted.get('url', 'https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
+        max_sent = posted.get('max_sent', 10)
+    else:
+        url = request.values.get('url', 'https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
+        max_sent = request.values.get('max_sent', 10)
     language = 'english'
     session_data = get_summary(url, max_sent, language)
     return jsonify(**session_data)

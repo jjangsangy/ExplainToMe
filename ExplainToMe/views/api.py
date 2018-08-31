3acfe3ffb49ada61f96a94603290bfd4c00d21b6
Sang Han <jjangsangy@gmail.com>
2018-08-30 21:14:57 -0700

    updated dependencies

--- ExplainToMe/views/api.py
+++ ExplainToMe/views/api.py
@@ -7,17 +7,10 @@ api = Blueprint('api', __name__)
 
 @api.route('/summary', methods=['POST'])
 def summary():
-<<<<<<< HEAD
     if request.is_json:
         data = request.get_json()
         url = data.get('url', 'https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
         max_sent = data.get('max_sent', 10)
-=======
-    posted = request.get_json()
-    if posted:
-        url = posted.get('url', 'https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
-        max_sent = posted.get('max_sent', 10)
->>>>>>> 339f4ce5dc3fea9ad34e0d93a2eb2c144b22230e
     else:
         url = request.values.get('url', 'https://www.wsj.com/articles/how-panera-solved-its-mosh-pit-problem-1496395801')
         max_sent = request.values.get('max_sent', 10)
